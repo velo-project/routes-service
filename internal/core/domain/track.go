d302a6b42f6cb7f7ceb54ce87e14e9872792b73e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-09-14 19:29:55 -0300

    feat: new properties of track.go

--- internal/core/domain/track.go
+++ internal/core/domain/track.go
@@ -1,15 +1,23 @@
 package domain
 
+import "time"
+
 type Track struct {
-	ID     *int       `json:"id,omitempty"`
-	UserID *int       `json:"user_id"`
-	Track  []Location `json:"track"`
+	ID              *int       `json:"id,omitempty"`
+	UserID          *int       `json:"user_id"`
+	InitialLocation string     `json:"initial_location"`
+	FinalLocation   string     `json:"final_location"`
+	VisitedAt       *time.Time `json:"visited_at"`
+	Track           []Location `json:"track"`
 }
 
-func NewTrack(id, userId *int, track []Location) *Track {
+func NewTrack(id, userId *int, initialLocation, finalLocation string, visitedAt *time.Time, track []Location) *Track {
 	return &Track{
-		ID:     id,
-		UserID: userId,
-		Track:  track,
+		ID:              id,
+		UserID:          userId,
+		InitialLocation: initialLocation,
+		FinalLocation:   finalLocation,
+		VisitedAt:       visitedAt,
+		Track:           track,
 	}
 }
