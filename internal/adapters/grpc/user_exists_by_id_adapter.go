a93bdbdf8d66b9fde02c5e6b8f053170599831a8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-09-16 20:29:46 -0300

    feat: user exists by id grpc adapter

---
+++ internal/adapters/grpc/user_exists_by_id_adapter.go
@@ -0,0 +1,34 @@
+package grpc
+
+import (
+	"gitlab.com/velo-company/services/routes-service/internal/core/ports"
+	"gitlab.com/velo-company/services/routes-service/proto/user"
+	"google.golang.org/grpc"
+)
+
+type userExistsByIdAdapter struct {
+	client user.UserServiceClient
+}
+
+func NewUserExistsByIdAdapter(connection *grpc.ClientConn) ports.UserExistsByIdPort {
+	return &userExistsByIdAdapter{
+		client: user.NewUserServiceClient(connection),
+	}
+}
+
+//func (u userExistsByIdAdapter) Execute(userId int) (bool, error) {
+//	res, err := u.client.UserExistsById(context.Background(), &user.UserExistsByIdRequest{
+//		Id: int32(userId),
+//	})
+//
+//	if err != nil {
+//		return false, err
+//	}
+//
+//	return res.Exists, nil
+//}
+
+// JUST FOR DEVELOPMENT
+func (u userExistsByIdAdapter) Execute(userId int) (bool, error) {
+	return true, nil
+}
